f1a27f49bd21bcec95e00e64975d8e330c8cfc53
jindalnishant <47566866+jindalnishant@users.noreply.github.com>
2020-04-25 18:01:52 +0530

    Add files via upload

---
+++ 303.RASA_Action_1/actions.py
@@ -0,0 +1,39 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+#
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+#from rasa_sdk.events import SlotSet
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+class ActionCheckWeather(Action):
+   def name(self) -> Text:
+      return "action_check_weather"
+
+   def run(self,
+           dispatcher: CollectingDispatcher,
+           tracker: Tracker,
+           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+      print("In the Action Check Weather")
+      dispatcher.utter_message("Hello World! from custom action")
+      return []
